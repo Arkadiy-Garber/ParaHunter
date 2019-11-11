c669a865238e26ffafb188b1acc475a601c8c90b
Arkadiy-Garber <arkg@udel.edu>
2019-11-10 19:05:15 -0700

    Add files via upload

---
+++ reformat-headers.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+import re
+import os
+import textwrap
+import argparse
+import sys
+
+
+def SUM(ls):
+    count = 0
+    for i in ls:
+        count += float(i)
+    return count
+
+
+def deExt(string):
+    ls = string.split(".")
+    ls2 = ls[0:len(ls) - 1]
+    outstring = "".join(ls2)
+    return outstring
+
+
+def unique(ls, ls2):
+    unqlist = []
+    for i in ls:
+        if i not in unqlist and i in ls2:
+            unqlist.append(i)
+    return len(unqlist)
+
+
+
+def derep(ls):
+    outLS = []
+    for i in ls:
+        if i not in outLS:
+            outLS.append(i)
+    return outLS
+
+
+def cluster(data, maxgap):
+    '''Arrange data into groups where successive elements
+       differ by no more than *maxgap*
+
+        #->>> cluster([1, 6, 9, 100, 102, 105, 109, 134, 139], maxgap=10)
+        [[1, 6, 9], [100, 102, 105, 109], [134, 139]]
+
+        #->>> cluster([1, 6, 9, 99, 100, 102, 105, 134, 139, 141], maxgap=10)
+        [[1, 6, 9], [99, 100, 102, 105], [134, 139, 141]]
+
+    '''
+    # data = sorted(data)
+    data.sort(key=int)
+    groups = [[data[0]]]
+    for x in data[1:]:
+        if abs(x - groups[-1][-1]) <= maxgap:
+            groups[-1].append(x)
+        else:
+            groups.append([x])
+    return groups
+
+
+def lastItem(ls):
+    x = ''
+    for i in ls:
+        x = i
+    return x
+
+
+def RemoveDuplicates(ls):
+    empLS = []
+    for i in ls:
+        if i not in empLS:
+            empLS.append(i)
+        else:
+            pass
+    return empLS
+
+
+def allButTheLast(iterable, delim):
+    x = ''
+    length = len(iterable.split(delim))
+    for i in range(0, length - 1):
+        x += iterable.split(delim)[i]
+        x += delim
+    return x[0:len(x) - 1]
+
+
+def secondToLastItem(ls):
+    x = ''
+    for i in ls[0:len(ls) - 1]:
+        x = i
+    return x
+
+
+def pull(item, one, two):
+    ls = []
+    counter = 0
+    for i in item:
+        if counter == 0:
+            if i != one:
+                pass
+            else:
+                counter += 1
+                ls.append(i)
+        else:
+            if i != two:
+                ls.append(i)
+            else:
+                ls.append(i)
+                counter = 0
+    outstr = "".join(ls)
+    return outstr
+
+
+def stabilityCounter(int):
+    if len(str(int)) == 1:
+        string = (str(0) + str(0) + str(0) + str(0) + str(int))
+        return (string)
+    if len(str(int)) == 2:
+        string = (str(0) + str(0) + str(0) + str(int))
+        return (string)
+    if len(str(int)) == 3:
+        string = (str(0) + str(0) + str(int))
+        return (string)
+    if len(str(int)) == 4:
+        string = (str(0) + str(int))
+        return (string)
+
+
+def replace(stringOrlist, list, item):
+    emptyList = []
+    for i in stringOrlist:
+        if i not in list:
+            emptyList.append(i)
+        else:
+            emptyList.append(item)
+    outString = "".join(emptyList)
+    return outString
+
+
+def remove(stringOrlist, list):
+    emptyList = []
+    for i in stringOrlist:
+        if i not in list:
+            emptyList.append(i)
+        else:
+            pass
+    outString = "".join(emptyList)
+    return outString
+
+
+def remove2(stringOrlist, list):
+    emptyList = []
+    for i in stringOrlist:
+        if i not in list:
+            emptyList.append(i)
+        else:
+            pass
+    # outString = "".join(emptyList)
+    return emptyList
+
+
+def removeLS(stringOrlist, list):
+    emptyList = []
+    for i in stringOrlist:
+        if i not in list:
+            emptyList.append(i)
+        else:
+            pass
+    return emptyList
+
+
+def fasta(fasta_file):
+    seq = ''
+    header = ''
+    Dict = defaultdict(lambda: defaultdict(lambda: 'EMPTY'))
+    for i in fasta_file:
+        i = i.rstrip()
+        if re.match(r'^>', i):
+            if len(seq) > 0:
+                Dict[header] = seq
+                header = i[1:]
+                header = header.split(" # ")[0]
+                seq = ''
+            else:
+                header = i[1:]
+                header = header.split(" # ")[0]
+                seq = ''
+        else:
+            seq += i
+    Dict[header] = seq
+    return Dict
+
+
+def filter(list, items):
+    outLS = []
+    for i in list:
+        if i not in items:
+            outLS.append(i)
+    return outLS
+
+
+def delim(line):
+    ls = []
+    string = ''
+    for i in line:
+        if i != " ":
+            string += i
+        else:
+            ls.append(string)
+            string = ''
+    ls = filter(ls, [""])
+    return ls
+
+
+parser = argparse.ArgumentParser(
+    prog="LithoGenie.py",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=textwrap.dedent('''
+    *******************************************************
+
+    Developed by Arkadiy Garber and Gustavo Ramírez;
+    University of Southern California, Earth Sciences
+    Please send comments and inquiries to arkadiyg@usc.edu
+    *******************************************************
+    '''))
+
+parser.add_argument('-i', type=str, help="input fasta")
+
+parser.add_argument('-o', type=str, help="output fasta")
+
+args = parser.parse_args()
+
+out = open(args.o, "w")
+file = open(args.i)
+for i in file:
+    if not re.match(r'>', i):
+        out.write(i.rstrip() + "\n")
+    else:
+        out.write(remove(i.rstrip(), ["|", "/", " ", ",", "&", "$", "!"]) + "\n")
+out.close()
+
+
+
+
+
+
+
+
